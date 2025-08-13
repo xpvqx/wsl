8cf79f099f0323b1ce1459c04de16c8e3b350a16
xpvqx <149103142+xpvqx@users.noreply.github.com>
2025-08-13 15:07:11 +0200

    Add files via upload

---
+++ nvim/lua/options.lua
@@ -0,0 +1,19 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.swapfile = false
+vim.o.wrap = false
+
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.autoindent = true
+
+vim.o.smartcase = true
+vim.o.ignorecase = true
+vim.o.hlsearch = false
+vim.o.incsearch = true
+
+vim.o.termguicolors = true
+vim.o.background = "dark"
